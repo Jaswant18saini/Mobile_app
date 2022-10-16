ff4c111ba0aaa16d02bb6abd4f1435feb1840a16
bk147411@gmail.com <bk147411@gmail.com>
2022-10-16 16:27:59 +0530

    Download,offline,dowmloadCheck

--- Screens/Home.js
+++ Screens/Home.js
@@ -5,24 +5,19 @@ import {
   Button,
   View,
   Text,
-  LogBox,
   ScrollView,
   StyleSheet,
-  Alert,
-  Pressable,
-  Modal,
   NativeModules,
 } from 'react-native';
 import {create} from 'apisauce';
 import {useNetInfo} from '@react-native-community/netinfo';
+import RNFS from 'react-native-fs';
 
-const DOCUMENT =
-  Platform.OS === 'ios' ? 'Document.pdf' : 'file:///android_asset/Document.pdf';
 const Home = () => {
   const netInfo = useNetInfo();
 
   const api = create({
-    baseURL: 'https://00e3-180-188-224-155.in.ngrok.io',
+    baseURL: 'https://e177-180-188-224-155.in.ngrok.io',
     headers: {Accept: 'application/json'},
   });
 
@@ -31,14 +26,7 @@ const Home = () => {
   const ref1 = useRef();
   var PSPDFKit = NativeModules.PSPDFKit;
 
-  const handlePress = id => {
-    PSPDFKit.present(DOCUMENT, {
-      showThumbnailBar: 'scrollable',
-      pageTransition: 'scrollContinuous',
-      scrollDirection: 'vertical',
-      documentLabelEnabled: true,
-    });
-  };
+  const handlePress = id => {};
   useEffect(() => {
     if (netInfo.type !== 'unknown' && netInfo.isInternetReachable === false) {
       get();
@@ -53,41 +41,98 @@ const Home = () => {
       .then(response => response?.data.tree)
       .then(async data => {
         setFolder(data);
-        await AsyncStorage.setItem('Demo', JSON.stringify(data));
+        await AsyncStorage.setItem('FolderInfo', JSON.stringify(data));
       });
   }
 
   const get = async key => {
-    try {
-      const value = await AsyncStorage.getItem('Demo');
-      const item = JSON.parse(value);
-      setFolder(item);
-      console.log('item', item);
-    } catch (err) {
-      console.log(err);
-    }
+    RNFS.readDir(RNFS.DocumentDirectoryPath)
+      .then(async result => {
+        console.log('GOT RESULT', result);
+        let datafromstorage = result?.filter(val => val.name.includes('.pdf'));
+        try {
+          const value = await AsyncStorage.getItem('FolderInfo');
+          const item = JSON.parse(value);
+          const updatedData = item?.map((val, index) => {
+            const filedata = datafromstorage?.find(data =>
+              data?.name.includes(val?.Id),
+            );
+            console.log('filedata', filedata);
+            if (filedata) {
+              val['download'] = true;
+            } else {
+              val['download'] = false;
+            }
+            return val;
+          });
+          console.log('updatedData', updatedData);
+          setFolder(updatedData);
+        } catch (err) {
+          console.log(err);
+        }
+        console.log('data', data);
+      })
+      .catch(err => {
+        console.log(err.message, err.code);
+      });
   };
+  const handleView = val => {
+    console.log('great!!!');
+    const documentNew =
+      Platform.OS === 'ios'
+        ? 'Document.pdf'
+        : `file://${RNFS.DocumentDirectoryPath}/${val?.Id}.pdf`;
+    PSPDFKit.present(documentNew, {
+      showThumbnailBar: 'scrollable',
+      pageTransition: 'scrollContinuous',
+      scrollDirection: 'vertical',
+      documentLabelEnabled: true,
+    });
+  };
+  const handleDownload = val => {
+    RNFS.downloadFile({
+      fromUrl: val?.url,
+      toFile: `${RNFS.DocumentDirectoryPath}/${val.Id}.pdf`,
+    }).promise.then(r => {
+      console.log('working', RNFS.DocumentDirectoryPath);
+      // this.setState({ isDone: true })
+    });
+  };
+  console.log('folder', folder);
 
   return (
-    <>
-      <View>
-        <ScrollView>
-          <View>
-            {folder?.map(person => {
-              return (
-                <View key={person.Id}>
-                  <Text
-                    onPress={() => handlePress(person.Id)}
-                    style={styles.item}>
-                    {person.File_Name__c}
-                  </Text>
-                </View>
-              );
-            })}
-          </View>
-        </ScrollView>
-      </View>
-    </>
+    <View>
+      <ScrollView>
+        <View>
+          {folder?.map(person => {
+            return (
+              <View key={person.Id}>
+                <Text
+                  onPress={() => handlePress(person.Id)}
+                  style={styles.item}>
+                  {person.File_Name__c}
+                </Text>
+                <Button
+                  disabled={!person?.download}
+                  title="View"
+                  onPress={() => handleView(person)}
+                />
+                <Button
+                  disabled={
+                    netInfo.type !== 'unknown' &&
+                    netInfo.isInternetReachable === false
+                  }
+                  title="Download"
+                  onPress={() => {
+                    handleDownload(person);
+                  }}
+                />
+              </View>
+            );
+          })}
+        </View>
+      </ScrollView>
+    </View>
   );
 };
 export default Home;
