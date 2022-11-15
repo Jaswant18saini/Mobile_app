df244f9f2bf412b5739792fee56fa28499fb0b24
bk147411@gmail.com <bk147411@gmail.com>
2022-11-16 00:33:53 +0530

    offline capability for Home

--- Screens/components/Documents/index.js
+++ Screens/components/Documents/index.js
@@ -1,67 +1,94 @@
 import React, {useEffect, useState} from 'react';
 import Icon from 'react-native-vector-icons/FontAwesome';
-import {
-  View,
-  Text,
-  Button,
-  FlatList,
-  StyleSheet,
-  StatusBar,
-  Image,
-} from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {View, Text, Button, FlatList, StyleSheet, Image} from 'react-native';
 import {TouchableOpacity} from 'react-native-gesture-handler';
 import {create} from 'apisauce';
 import Header from './Header';
-import {createThumbnail} from 'react-native-create-thumbnail';
+import {useNetInfo} from '@react-native-community/netinfo';
+import RNFS from 'react-native-fs';
 import {ScrollView} from 'react-native';
 
 const Documents = () => {
   const [parentFolder, setParentFolder] = useState();
   const [folderData, setFolderData] = useState();
   const [fileData, setFileData] = useState();
+
+  const netInfo = useNetInfo();
+
   const api = create({
     baseURL: 'http://34.231.129.177',
     headers: {Accept: 'application/json'},
   });
 
   function Allfolder() {
-    api
-      .get('/folder?projectId=a0f0r000000vIrEAAU')
-      .then(res => setParentFolder(res?.data?.tree?.children));
+    api.get('/folder?projectId=a0f0r000000vIrEAAU').then(async res => {
+      setParentFolder(res?.data?.tree?.children);
+      await AsyncStorage.setItem(
+        'AllFolders',
+        JSON.stringify(res?.data?.tree?.children),
+      );
+    });
   }
 
   useEffect(() => {
     Allfolder();
   }, []);
-  // console.log('parentFolder', parentFolder);
-  // console.log('folderData', folderData);
+  const checkNet = async () => {
+    console.log('herererere123');
+    const value = await AsyncStorage.getItem('AllFolders');
+    console.log('itemmm', value);
+    item = JSON.parse(value);
+
+    setParentFolder(item);
+  };
+
+  useEffect(() => {
+    if (netInfo.type !== 'unknown' && netInfo.isInternetReachable === false) {
+      checkNet();
+    } else {
+      Allfolder();
+    }
+  }, [netInfo.type, netInfo.isInternetReachable]);
+
   console.log('fileData', fileData);
 
-  const handleParentFolder = item => {
+  const handleParentFolder = async item => {
     setFolderData(item?.children);
 
-    api.get(`/folderfiles/${item?.value?.Id}`).then(res => {
-      res?.data?.tree?.map(async item => {
-        return (item.thumbnail = '');
+    if (netInfo.type !== 'unknown' && netInfo.isInternetReachable === false) {
+      const value = await AsyncStorage.getItem('FoldersFiles');
+      console.log('itemmm', value);
+      item = JSON.parse(value);
+
+      setFileData(item);
+    } else {
+      api.get(`/folderfiles/${item?.value?.Id}`).then(async res => {
+        res?.data?.tree?.map(async item => {
+          return (item.thumbnail = '');
+        });
+        setFileData(res?.data?.tree);
+        await AsyncStorage.setItem(
+          'FoldersFiles',
+          JSON.stringify(res?.data?.tree),
+        );
       });
-      setFileData(res?.data?.tree);
-    });
+    }
   };
 
   const Buttons = ({item}) => {
-    console.log("item",item);
+    console.log('item', item);
     return (
       <View
         key={item?.value?.Id}
-        style={{ 
-          padding: 10,  width: '49%',
-          marginLeft: 'auto', 
+        style={{
+          padding: 10,
+          width: '49%',
+          marginLeft: 'auto',
           marginRight: 'auto',
-        }}
-        >
-          
-        <Button 
-          title={item?.value?.Name}        
+        }}>
+        <Button
+          title={item?.value?.Name}
           onPress={() => handleParentFolder(item)}
         />
       </View>
@@ -70,15 +97,17 @@ const Documents = () => {
 
   const FolderView = ({item}) => {
     return (
-      <View style={styles.buttonDown}>
-        <TouchableOpacity onPress={null}>
-          <Icon type="FontAwesome" name="folder" color="#000" />
-        </TouchableOpacity>
-        <Text>{item?.value?.Name}</Text>
-        <TouchableOpacity onPress={null}>
-          <Icon type="FontAwesone" name="download" color="#000" />
-        </TouchableOpacity>
-      </View>
+      <ScrollView>
+        <View style={styles.buttonDown}>
+          <TouchableOpacity onPress={null}>
+            <Icon type="FontAwesome" name="folder" color="#000" />
+          </TouchableOpacity>
+          <Text>{item?.value?.Name}</Text>
+          <TouchableOpacity onPress={null}>
+            <Icon type="FontAwesone" name="download" color="#000" />
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
     );
   };
 
@@ -87,13 +116,34 @@ const Documents = () => {
       image: require('../../../assets/thumbnailDemo.webp'),
     };
     return (
-      // <ScrollView>
+      <ScrollView>
         <View style={styles.Boxwrapper}>
-          <Text style={[styles.textName,{fontWeight: '900', color: '#333'}]} >{item?.Name}</Text>
-          <Image source={data?.image} style={[{width: 100, height: 100 ,marginLeft: 'auto',marginRight: 'auto',flexWrap: 'wrap',alignItems: 'center',flex: 1,justifyContent: 'center'}]} />
-          <Icon style={{textAlign: 'center', marginTop: 15,marginBottom: 10,}} type="FontAwesone" name="download" color="#000" />
+          <Text style={[styles.textName, {fontWeight: '900', color: '#333'}]}>
+            {item?.Name}
+          </Text>
+          <Image
+            source={data?.image}
+            style={[
+              {
+                width: 100,
+                height: 100,
+                marginLeft: 'auto',
+                marginRight: 'auto',
+                flexWrap: 'wrap',
+                alignItems: 'center',
+                flex: 1,
+                justifyContent: 'center',
+              },
+            ]}
+          />
+          <Icon
+            style={{textAlign: 'center', marginTop: 15, marginBottom: 10}}
+            type="FontAwesone"
+            name="download"
+            color="#000"
+          />
         </View>
-      // </ScrollView>
+      </ScrollView>
     );
   };
 
@@ -101,33 +151,37 @@ const Documents = () => {
     <>
       <View>
         <Header />
-        <View >
-          <FlatList style={styles.button}
+        <View>
+          <FlatList
+            style={styles.button}
             data={parentFolder}
             renderItem={Buttons}
-            numColumns={2} horizontal={false}
-            keyExtractor={item => item.value.Id} 
+            numColumns={2}
+            horizontal={false}
+            keyExtractor={item => item.value.Id}
           />
         </View>
       </View>
       <View>
         <View>
-          <Text style={{textAlign: 'center',marginBottom: 10,}}>Folders</Text>
+          <Text style={{textAlign: 'center', marginBottom: 10}}>Folders</Text>
           <View style={styles.mainBx}>
-          <FlatList  
-            data={folderData}
-            numColumns={2}
-            horizontal={false}
-
-            renderItem={FolderView}
-            keyExtractor={item => item.value.Id}
-          />
+            <FlatList
+              data={folderData}
+              numColumns={4}
+              horizontal={false}
+              renderItem={FolderView}
+              keyExtractor={item => item.value.Id}
+            />
           </View>
         </View>
-        <View style={{width:"100%", paddingHorizontal: 10}}>
-          <Text style={{textAlign: 'center', marginTop: 15,marginBottom: 10,}}>Documents</Text>
+        <View style={{width: '100%', paddingHorizontal: 10}}>
+          <Text style={{textAlign: 'center', marginTop: 15, marginBottom: 10}}>
+            Documents
+          </Text>
           <FlatList
-            data={fileData} numColumns={2}
+            data={fileData}
+            numColumns={4}
             horizontal={false}
             renderItem={FilesView}
             keyExtractor={item => item.Id}
@@ -160,33 +214,40 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     fontSize: 30,
     fontWeight: 'bold',
-  }, 
-  mainBx:{ 
-    alignItems: "center",  
-    flexDirection: "row",
-    justifyContent: "center",    flexWrap: "wrap",  gap: '10px',
   },
-  buttonDown:{
-    backgroundColor: '#f7f7f7', 
+  mainBx: {
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    flexWrap: 'wrap',
+    gap: '10px',
+  },
+  buttonDown: {
+    backgroundColor: '#f7f7f7',
     marginLeft: 0,
-    padding: 10, 
+    padding: 10,
     flexBasis: 50,
-    flexDirection: 'row', 
+    flexDirection: 'row',
     justifyContent: 'space-between',
-     flex: 1,
-    marginHorizontal: 20, margin: 4,
-  }, 
-  Boxwrapper: {  
-    flexDirection: 'column',  
-    borderWidth: 1,padding: 10,
+    flex: 1,
+    marginHorizontal: 20,
+    margin: 4,
+  },
+  Boxwrapper: {
+    flexDirection: 'column',
+    borderWidth: 1,
+    padding: 10,
     borderColor: '#ccc',
     borderStyle: 'solid',
-    gap: '5px',flex: 1, 
-    justifyContent: 'center',flex: 1,
-    width: '100%',  margin: 10
+    gap: '5px',
+    flex: 1,
+    justifyContent: 'center',
+    flex: 1,
+    width: '100%',
+    margin: 10,
   },
-  textName:{
-    textAlign: 'center',    
-  }
+  textName: {
+    textAlign: 'center',
+  },
 });
 export default Documents;
