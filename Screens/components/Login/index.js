19c0c0cbfd34834d35de4d3a3c1958e847ed9930
Raghav s <raghavsimlote@gmail.com>
2022-12-05 17:06:38 +0530

    android login changes

--- Screens/components/Login/index.js
+++ Screens/components/Login/index.js
@@ -4,8 +4,10 @@ import {WebView} from 'react-native-webview';
 
 const Login = () => {
   // console.log(AUTH_URL);
-  const AUTH_URL =
-    'https://login.salesforce.com/services/oauth2/authorize?response_type=token&client_id=3MVG9wt4IL4O5wvKO2JUZnYYNyI7XJuNh_ZPFmHs1gHytxLtJq3t.tqpfDC8_rlXh50Xngz0R9xBR_HhMZwat&redirect_uri=http://localhost:3002';
+  const CLIENT_ID = '3MVG9RezSyZYLh2uCLY.LsKGxySkKzZBiHV8luYqMgeeI77I5jfamuycuMsqiL_XOZqqab.Aqvw==';
+  const CALLBACK = encodeURIComponent('com.pspdfkitdemo://oauth');
+  const AUTH_URL = 'https://test.salesforce.com/services/oauth2/authorize?response_type=token&client_id=' + CLIENT_ID + '&redirect_uri=' + CALLBACK;
+    console.log("AUTH_URL",AUTH_URL);
   return (
     <>
       {AUTH_URL && (
